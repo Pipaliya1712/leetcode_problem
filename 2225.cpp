349b00c482113ed65bd29da4bf68c76bbf30bf73
Parth Pipaliya <parthpipaliya1712@gmail.com>
2024-01-15 15:30:13 +0530

    leetcode problem 2225

---
+++ 2225.cpp
@@ -0,0 +1,86 @@
+/*
+2225. Find Players With Zero or One Losses ( Medium )
+
+You are given an integer array matches where matches[i] = [winneri, loseri] indicates that the player winneri defeated player loseri in a match.
+Return a list answer of size 2 where:
+answer[0] is a list of all players that have not lost any matches.
+answer[1] is a list of all players that have lost exactly one match.
+The values in the two lists should be returned in increasing order.
+
+Note:
+You should only consider the players that have played at least one match.
+The testcases will be generated such that no two matches will have the same outcome.
+ 
+
+Example 1:
+Input: matches = [[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]]
+Output: [[1,2,10],[4,5,7,8]]
+Explanation:
+Players 1, 2, and 10 have not lost any matches.
+Players 4, 5, 7, and 8 each have lost one match.
+Players 3, 6, and 9 each have lost two matches.
+Thus, answer[0] = [1,2,10] and answer[1] = [4,5,7,8].
+
+Example 2:
+Input: matches = [[2,3],[1,3],[5,4],[6,4]]
+Output: [[1,2,5,6],[]]
+Explanation:
+Players 1, 2, 5, and 6 have not lost any matches.
+Players 3 and 4 each have lost two matches.
+Thus, answer[0] = [1,2,5,6] and answer[1] = [].
+ 
+
+Constraints:
+
+1 <= matches.length <= 105
+matches[i].length == 2
+1 <= winneri, loseri <= 105
+winneri != loseri
+All matches[i] are unique.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> findWinners(vector<vector<int>>& matches) {
+    unordered_map<int,int>lost_map;
+    for(int i=0;i<matches.size();i++){
+        lost_map[matches[i][1]]++;
+    }
+    vector<int>notLost;
+    vector<int>onceLost;
+    for(int i=0;i<matches.size();i++){
+       if(lost_map.find(matches[i][0])==lost_map.end()){
+           notLost.push_back(matches[i][0]);
+           lost_map[matches[i][0]]=2;
+       }
+       if(lost_map[matches[i][1]]==1)
+       onceLost.push_back(matches[i][1]);
+    }
+    sort(notLost.begin(),notLost.end());    
+    sort(onceLost.begin(),onceLost.end());
+    return {notLost,onceLost}; 
+}
+
+int main(){
+    vector<vector<int>> matches(10);
+    int n;cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<2;j++){
+            int x;
+            cin>>x;
+            matches[i].push_back(x);
+        }
+    }
+    vector<vector<int>> ans = findWinners(matches);
+    cout<<ans[0].size()<<endl;
+    cout<<ans[1].size()<<endl;
+    for(int i=0;i<ans[0].size();i++){
+        cout<<ans[0][i]<<" ";
+    }
+    cout<<endl;
+    for(int i=0;i<ans[1].size();i++){
+        cout<<ans[1][i]<<" ";
+    }
+    return 0;
+}
